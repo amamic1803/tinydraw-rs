454889f236cb5be8e2db9cb0f15b8e508ff35a83
Antonio Mamić <40371578+DarkLord76865@users.noreply.github.com>
2023-02-12 22:11:57 +0000

    testing commit from codespace

--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,8 @@ mod tests {
         let mut img = ImageRGB8::new(200, 200, [25, 200, 25]);
         img.clear();
         //img.draw_circle(100, 100, 11, [255, 255, 255], 0, 1.0);
-        // img.draw_circle(100, 100, 20, [255, 255, 255], 0, 1.0);
-        img.draw_line(50, 50, 125, 150, [255, 255, 255], 1, 0.25);
+        img.draw_circle(100, 100, 20, [255, 255, 255], 0, 1.0);
+        // img.draw_line(50, 50, 125, 150, [255, 255, 255], 1, 0.25);
         img.to_png("test_image3.png").unwrap();
     }
 }
